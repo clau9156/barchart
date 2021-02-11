269ff37161442252e3d42fec24b23bb991b1edd5
clau9156 <clau9156@stud.kea.dk>
2021-02-11 22:07:16 +0100

    first line changing size but fast BUT WORKS

--- barchart.js
+++ barchart.js
@@ -1,33 +1,45 @@
 "use strict";
-
-const queueSize = getNumberofCustomers();
+// array (random numbers - model(40 elements))
+// const queueSize = getNumberofCustomers();
+let bar = document.querySelector(".bar");
 let bars = document.querySelector("#bars");
 
 window.addEventListener("load", init);
 
 function init() {
-
+    console.log("init"); 
     loop();
+    // displayData();
 }
-
+// i = measurements
+// if to loop
 function loop() {
-    setTimeout(loop, 1000);
-    getNumberofCustomers();
+    console.log("loop");
+    for (let i=0; i<40;) {
+        i++;
+        bar.style.height = i + "%";
+    
+    }
 }
 
 function getNumberofCustomers() {
+    console.log("getNumberofCustomers");
     // FAKE: gives a completely random number
     return Math.floor(Math.random()*32);
-    
-    displayData();
 }
 
 function displayData() {
-    queueSize.shift(bars);
+    console.log("displayData");
+    bars.appendChild(bar);
     modifyModel();
+    setTimeout(displayData, 1000);
 }
 
 function modifyModel() {
-
-    console.log(modifyModel);
+    console.log("modifyModel");
+    const queueSize = getNumberofCustomers();
+    // adds at the end
+    bars.push();
+    // scroll left
+    bars.shift();
 }
